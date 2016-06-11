1dafe027f5d06aea2205bf716747fd76dee27b02
relative-rene <rene.arellano415@gmail.com>
2016-06-11 15:28:57 -0700

    refactored Events

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ app.get('/api', function api_index(req, res) {
     base_url: "https://github.com/relative-rene/express-personal-api", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/events", description: "data for all upcoming Event"},
-      {method: "GET", path: "/api/events/:id", description: "data for specific upcoming Event"},
-      {method: "POST", path: "/api/events", description: "creating new Event"},
-      {method: "DELETE", path: "/api/events/:id", description: "canceling/removing specific Event"},
+      {method: "GET", path: "/api/events", description: "data for all upcoming Events"},
+      {method: "GET", path: "/api/events/:id", description: "data for specific upcoming Events"},
+      {method: "POST", path: "/api/events", description: "creating new Events"},
+      {method: "DELETE", path: "/api/events/:id", description: "canceling/removing specific Events"},
       {method: "GET", path: "/api/profiles", description: "retrieve all profiles"},
       {method: "GET", path: "/api/profiles/:id", description: "retrieve specific profile"}]
     });
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 // get all profiles
 app.get('/api/profiles', function (req,res) {
   //send all profiles as json res
-  db.Event.find().populate('profiles').exec(function(err,profile){
+  db.Events.find().populate('profiles').exec(function(err,profile){
     if(err){ return console.log("index error: " +err);
   }
     res.json(profile);
@@ -81,27 +81,27 @@ app.get('/api/profiles/:id', function (req, res) {
 
 
 
-// get all Event
+// get all Events
 app.get('/api/events', function (req,res) {
-  //send all Event as json res
-  db.Event.find().populate('Event').exec(function(err,Event){
+  //send all Events as json res
+  db.Events.find().populate('Events').exec(function(err,Events){
     if(err){ return console.log("index error: " +err);
   }
-    res.json(Event);
+    res.json(Events);
   });
 });
 
-// get one Event
+// get one Events
 app.get('/api/events/:id', function (req, res) {
-  db.Event.findOne({_id: req.params._id }, function(err, data) {
+  db.Events.findOne({_id: req.params._id }, function(err, data) {
     res.json(data);
   });
 });
 
-// create new Event
+// create new Events
 app.post('/api/events', function (req, res) {
   // create new book with form data (`req.body`)
-  var newEvent = new db.Event({
+  var newEvent = new db.Events({
     name: req.body.name,
     link: req.body.link,
     image: req.body.image,
@@ -109,32 +109,32 @@ app.post('/api/events', function (req, res) {
     pets: req.body.pets
   });
   // find the author from req.body
-  db.Event.findOne({name: req.body.Event}, function(err, Event){
+  db.Events.findOne({name: req.body.Events}, function(err, Events){
     if (err) {
       return console.log(err);
     }
     // add this author to the bookz
-    newEvent.Event = Event;
+    newEvent.Events = Events;
 
     // save newEvent to database
-    newEvent.save(function(err, Event){
+    newEvent.save(function(err, Events){
       if (err) {
         return console.log("save error: " + err);
       }
-      console.log("saved ", Event.title);
-      // send back the Event!
-      res.json(Event);
+      console.log("saved ", Events.title);
+      // send back the Events!
+      res.json(Events);
     });
   });
 });
 
-// delete Event
+// delete Events
 app.delete('/api/events/:id', function (req, res) {
-  // get Event id from url params (`req.params`)
+  // get Events id from url params (`req.params`)
   console.log('event delete', req.params);
   var EventId = req.params.id;
   // find the index of the book we want to remove
-  db.Event.findOneAndRemove({ _id: EventId }, function (err, deletedEvent) {
+  db.Events.findOneAndRemove({ _id: EventId }, function (err, deletedEvent) {
     res.json(deletedEvents);
   });
 });
