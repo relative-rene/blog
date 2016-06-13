f26ffa29d926340316cedbd6943a73d1a1f7f7c4
relative-rene <rene.arellano415@gmail.com>
2016-06-12 21:53:59 -0700

    events module export

--- models/events.js
+++ models/events.js
@@ -1,13 +1,10 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
-  Events = require('./events');
-
-
 
 var EventSchema = new Schema({
-  Goal: String,
-  DueDate: String,
-  Summary: String
+  date: String,
+  topic: String,
+  post: String
 });
 var Events = mongoose.model('Events', EventSchema);
 module.exports = Events;
