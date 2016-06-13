a3894f24a51e6fe665f0886f06d95d1ecfc0c702
relative-rene <rene.arellano415@gmail.com>
2016-06-13 14:08:15 -0700

    seed data for first profile

--- seed.js
+++ seed.js
@@ -1,10 +1,11 @@
 //  This file allows us to seed our application with data
 //  simply run: `node seed.js` from the root of this project folder.
 //
-// var db = require('./models');
-//
+var db = require('./models');
+
 // var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
-//
+ var new_profile = {name: "Rene Arellano", github_link: "https://github.com/relative-rene", image: "/public/images/faceBookPic.jpg", city: "San Francisco"};
+
 //  db.Campsite.create(new_campsite, function(err, campsite){
 //    if (err){
 //      return console.log("Error:", err);
@@ -13,3 +14,10 @@
 //    console.log("Created new campsite", campsite._id)
 //    process.exit(); // we're all done! Exit the program.
 //  })
+  db.Profile.create(new_profile, function(err, profle){
+    if (err){
+      return console.log("Error:", err);
+    }
+    console.log("Created new profile", profile._id);
+    process.exit();
+  });
