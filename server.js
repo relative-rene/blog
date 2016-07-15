74128978c11a0b45ae7f3bb7647f87cc44b691ad
relative-rene <rene.arellano415@gmail.com>
2016-07-14 17:18:01 -0700

    refactored server for bower

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-// require express and other modules
-var express = require('express'),
-    db = require('./models'),
-    app = express(),
-    bodyParser = require('body-parser');
+// SERVER-SIDE JAVASCRIPT
 
-
-// Serve static files from the `/public` directory:
-// i.e. `/images`, `/scripts`, `/styles`
-app.use(express.static('public'));
-// parse incoming urlencoded form data
-// and populate the req.body object
+//require express in our app
+var express = require('express');
+// generate a new express app and call it 'app'
+var app = express();
+var bodyParser = require('body-parser');
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+// need to add this so that we can accept request payloads
+app.use(bodyParser.json());
+
+// We'll serve jQuery an
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
