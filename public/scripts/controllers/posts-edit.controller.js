25d753edadab635ff888b411dc07aff43157c780
relative-rene <rene.arellano415@gmail.com>
2016-08-18 17:30:32 -0700

    [ctrl] deletes old controllers consolidates delete and edit, probably broken functions

---
+++ public/scripts/controllers/posts-edit.controller.js
@@ -0,0 +1,34 @@
+angular
+  .module('blog')
+  .controller('PostsEditController', PostsEditController);
+
+  PostsIndexController.$inject = ['$http'];
+  function PostsIndexController ($http) {
+    var vm = this;
+    vm.post = {};
+
+  vm.edit = function (post) {
+    $http({
+      method: 'PUT',
+      url: '/api/posts/'+post._id,
+      data: post
+    }).then(function successCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response);
+    });
+  };
+
+  vm.deletePost = function (post) {
+    $http({
+      method: 'DELETE',
+      url: '/api/posts/'+ post._id
+    }).then(function successCallback(json) {
+      var index = vm.posts.indexOf(post);
+      vm.posts.splice(index,1);
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  };
+
+}
