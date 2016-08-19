93936e4c2f6e86891f20eaf828bedc9e347ad4d2
relative-rene <rene.arellano415@gmail.com>
2016-08-18 17:24:47 -0700

    [ctrl] R&R show, adds get function

---
+++ public/scripts/controllers/posts-show.controller.js
@@ -0,0 +1,25 @@
+angular
+  .module('blog')
+  .controller('PostsShowController', PostsShowController);
+
+PostsIndexController.$inject = ['$http'];
+function PostsShowController ($http) {
+  var vm = this;
+  vm.post = {};
+
+//  app.get('/api/posts/:Id', controllers.posts.show);
+  var id = $routeParams.id;
+  get();
+
+  vm.get = function (){
+    $http({
+      method: 'SHOW',
+      url: '/api/posts',
+      data: vm.Post,
+    }).then(function successCallback(res) {
+      vm.posts.push(res.data);
+    }, function errorCallback(res) {
+      console.log('There was an error posting ');
+    });
+  };
+}
