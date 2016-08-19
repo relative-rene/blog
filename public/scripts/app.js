c1c08b38b3a32589d948b2a136a77e215525636f
relative-rene <rene.arellano415@gmail.com>
2016-08-19 01:05:35 -0700

    [CTRL] cleaned up app.js files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,46 +1,10 @@
-//Client-side JS front end routing for space
-$(function(){
-  console.log('app.js loaded');
-});
-
+console.log('js running');
 angular
   .module('blog', ['ngRoute'])
+
+  //Posts CRUD controllers
+  .controller('PostsIndexController', PostsIndexController)
+  .controller('PostsShowController', PostsShowController)
+  .controller('PostsNewController', PostsNewController)
+  .controller('PostsEditController', PostsEditController)
   .config(config);
-
-config.$inject = ['$routeProvider', '$locationProvider'];
-function config ($routeProvider, $locationProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'templates/main/index.html',
-      controllerAs: 'postIndexCtrl',
-      controller: 'PostIndexController'
-    })
-    // Routes to POST //
-    .when('/posts', {
-      templateUrl: 'templates/main/index.html',
-      controllerAs: 'postIndexCtrl',
-      controller: 'PostIndexController'
-    })
-    .when('/posts/new', {
-      templateUrl: 'templates/posts/new.html',
-      controllerAs: 'postsNewCtrl',
-      controller: 'PostsNewController'
-    })
-    .when('/posts/:id', {
-      templateUrl: 'templates/posts/show.html',
-      controllerAs: 'postsShowCtrl',
-      controller: 'PostsShowController'
-    })
-    .when('/posts/:id/edit', {
-      templateUrl: 'templates/posts/edit.html',
-      controllerAs: 'postsEditCtrl',
-      controller: 'PostsEditController'
-    })
-    .otherwise({redirectTo: '/'});
-
-
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-  });
-}
