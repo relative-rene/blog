ae7ae567cf6523e126fdc199691f7bbfa93c0474
relative-rene <rene.arellano415@gmail.com>
2016-08-19 18:40:27 -0700

    [chore] refactors CRUD

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,62 +1,69 @@
-/************
- * DATABASE *
- ************/
-
 var db = require('../models');
 
-
-
-// GET /api/post
 function index(req, res) {
-  db.Post.find({}, function(err, allPosts) {
-    res.json(allPosts);
-  });
-}
-
-function create(req, res) {
-  console.log('body', req.body);
-
-  db.Post.create(req.body, function(err, post) {
-    if (err) { console.log('error', err); }
-    console.log(post);
-    res.json(post);
-  });
-}
-
-function show(req, res) {
-  db.Post.findById(req.params.postId, function(err, foundPost) {
-    if(err) { console.log('postController.show error', err); }
-    console.log('postController.show responding with', foundPost);
-    res.json(foundPost);
-  });
-}
-
-function destroy(req, res) {
-  db.Post.findOneAndRemove({ _id: req.params.postId }, function(err, foundPost){
-    // note you could send just send 204, but we're sending 200 and the deleted entity
-    res.json(foundPost);
-  });
-}
-
-function update(req, res) {
-  console.log('updating with data', req.body);
-  db.Post.findById(req.params.postId, function(err, foundPost) {
-    if(err) { console.log('postController.update error', err); }
-    foundPost.title = req.body.title;
-    foundPost.content = req.body.content;
-    foundPost.image = req.body.image;
-    foundPost.postLength = req.body.postLength;
-    foundPost.save(function(err, savedPost) {
-      if(err) { console.log('saving altered post failed'); }
-      res.json(savedPost);
+  db.Post
+    .find({})
+    .exec(function(err, posts){
+      if (err || !posts || !posts.length) {
+        return res.status(404).send({message: 'Posts not found.'});
+      }
+      res.send(posts);
     });
+}
+
+function create(req, res){
+  var new_post = new Post(req.body);
+  new_post.save(function(err, new_post){
+    res.send(new_post);
   });
 }
 
- module.exports = {
- index: index,
- create: create,
- show: show,
- destroy: destroy,
- update: update
- };
+function show(req, res){
+  db.Post
+    .findById(req.params.id)
+    .exec(function(err, found_post){
+      if (err || !found_post) {
+        return res.status(404).send({message: 'Post not found.'});
+      }
+      res.send(found_post);
+    });
+}
+
+function update(req, res){
+  var query = {
+    _id: req.params.id
+  };
+
+  db.Post
+    .findOneAndUpdate(query, req.body)
+    .exec(function(err, post){
+      if (err || !post) {
+        console.log(post);
+        return res.status(404).send({messsage: 'Failed to update post.'});
+      }
+      res.status(204).send();
+    });
+}
+
+function destroy(req, res){
+  var query = {
+    _id: req.params.id
+  };
+
+  db.Post
+    .findOneAndRemove(query)
+    .exec(function(err, post){
+      if (err || !post) {
+        return res.status(404).send({messsage: 'Failed to delete post.'});
+      }
+      res.status(204).send(query);
+    });
+}
+
+module.exports = {
+  index: index,
+  create: create,
+  show: show,
+  update: update,
+  destroy: destroy
+};
